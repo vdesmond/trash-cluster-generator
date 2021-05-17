e65f9d0aa8eac570f7945a2c11ade50c83c706fa
Vignesh Desmond <vignesh.desmond@gmail.com>
2021-05-17 15:50:12 +0530

    error handlers

---
+++ cluster_error.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Error(Exception):
+    """Base class for other exceptions"""
+
+    pass
+
+
+class OutOfBoundsClusterError(Error):
+    """Raised when the Cluster is out of bounds of dimensions of the image"""
+
+    pass
+
+
+class ClusterNotGeneratedError(Error):
+    """Raised when Add Cluster is called before Generate Cluster"""
+
+    pass
